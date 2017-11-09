7ba435d3749b4d22e0953fbe155f508f673b876a
Raju Gangadhar <geetagangadharraju@gmail.com>
2017-11-10 00:16:38 +0530

    The initial application. Add components to this skeleton to scale the application.

---
+++ src/components/list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                This is the list that will be displayed by default
+            </div>
+        );
+    }
+}
+
+export default List;
