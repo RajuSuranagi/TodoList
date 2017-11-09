adf9065ab2acb3be0eaa38dd0c2f73197e7a0e38
Raju Gangadhar <geetagangadharraju@gmail.com>
2017-11-10 01:16:52 +0530

    Adding redux state, action and default view

--- src/components/list.js
+++ src/components/list.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
+
+import { getList } from '../actions/index.js';
 
 class List extends Component {
 
     constructor(props){
         super(props);
+
+    }
+
+    componentDidMount(){
+        this.props.getList();
+    }
+
+    getTodoList(){
+        const items = this.props.items;
+        if(items == null || items.length == 0){
+            return (
+                <div>
+                    Add a todo item to start with.
+                </div>
+            )
+        } else {
+            items.map( (item) => {
+                return (
+                    <li key={item.id} className="list-group-item">
+                        <strong>{item.title}</strong>
+                        <span>{item.description}</span>
+                    </li>
+                )
+            })
+        }
     }
 
     render(){
         return (
             <div>
-                This is the list that will be displayed by default
+                {this.getTodoList()}
             </div>
         );
     }
 }
 
-export default List;
+function mapStateToProps(state){
+    return { items: state.list.items }
+}
+
+export default connect(mapStateToProps, {getList: getList })(List);
